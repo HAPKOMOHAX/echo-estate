9b2aefc5fd05e623bf69d6ef5c624e390e2bed9e
HAPKOMOHAX <mihakitaew99@gmail.com>
2026-06-08 19:47:10 +0500

    routes fix

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ Route::get('/', function () {
     return Inertia::render('Properties/Index');
 })->name('properties.index');
 
-Route::get('/properties', function () {
-    return redirect()->route('properties.index');
-});
-
 Route::get('/properties/search', [PropertySearchController::class, 'index'])
     ->name('properties.search');
 
